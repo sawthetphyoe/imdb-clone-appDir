af00a7198f4777b09918ea32316eadb023b7ae11
sawthetphyoe <sawthetphyoe28498@gmail.com>
2023-02-13 01:51:47 +0630

    added api and fetched movie data from TMDb

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,23 @@
-import Image from "next/image";
-import { Inter } from "@next/font/google";
+import Results from "@/components/Results";
 
-const inter = Inter({ subsets: ["latin"] });
+const API_KEY = process.env.API_KEY;
 
-export default function Home() {
-  return <h1 className="text-red-400">Home Page</h1>;
+async function fetchData(genre) {
+  const url = `https://api.themoviedb.org/3/${
+    genre === "fetchTopRated" ? "movie/top_rated" : "trending/all/week"
+  }?api_key=${API_KEY}&language=en-US&page=1`;
+
+  const res = await fetch(url, { next: { revalidate: 10000 } });
+
+  return res.json();
+}
+
+export default async function Home({ searchParams }) {
+  const genre = searchParams.genre || "fetchTrending";
+  const data = await fetchData(genre);
+  return (
+    <div>
+      <Results results={data.results} />
+    </div>
+  );
 }
