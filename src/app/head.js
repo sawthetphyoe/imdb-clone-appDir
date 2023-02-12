b37b068b567dd7387ed25e0191b331db0f81264c
sawthetphyoe <sawthetphyoe28498@gmail.com>
2023-02-12 19:00:09 +0630

    initial set up

---
+++ src/app/head.js
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>IMDB Clone</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="IMDB Clone" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
