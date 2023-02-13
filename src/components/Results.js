f1af7e344f6f49378cf2d03ab9c30700b89544c5
sawthetphyoe <sawthetphyoe28498@gmail.com>
2023-02-14 01:49:57 +0630

    fixed search input

--- src/components/Results.js
+++ src/components/Results.js
@@ -2,7 +2,7 @@ import MovieCard from "./MovieCard";
 
 export default function Results({ results }) {
   return (
-    <div className="sm:gap-4 sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-7xl mx-auto py-4 px-4 sm:px-8">
+    <div className="sm:gap-4 sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 max-w-7xl mx-auto py-4 px-8">
       {results.map((result) => (
         <MovieCard key={result.id} data={result} />
       ))}
